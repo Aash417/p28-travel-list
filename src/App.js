53941a23251c9878f411f547c5cf3251c87b8fdd
aashish_k <aashishkathat00@gmail.com>
2023-11-02 01:48:53 +0530

    controled elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
 import "./index.css";
+
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 export default function App() {
 	return (
@@ -16,20 +22,77 @@ function Logo() {
 }
 
 function Form() {
+	const [description, setDescripton] = useState("");
+	const [quantity, setQuantitiy] = useState(1);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		if (!description) return;
+
+		const newItem = {
+			description,
+			quantity,
+			packed: false,
+			id: Date.now(),
+		};
+		console.log(newItem);
+
+		setDescripton("");
+		setQuantitiy(1);
+	}
+
 	return (
-		<div className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your Trip?</h3>
-		</div>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantitiy(Number(e.target.value))}
+			>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescripton(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
 	);
 }
 
 function PackingList() {
-	return <div className="list">List</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item item={item} key={item.id} />
+				))}
+			</ul>
+		</div>
+	);
 }
+
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
+}
+
 function Stats() {
 	return (
 		<footer className="stats">
-			Your have X items on your list , and you already packedc X
+			Your have X items on your list , and you already packed X
 		</footer>
 	);
 }
