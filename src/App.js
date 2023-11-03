593fa8c061492c1369d0e1d296bbc91f755691a0
aashish_k <aashishkathat00@gmail.com>
2023-11-03 23:58:46 +0530

    lift up state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+	const [items, setItems] = useState([]);
+
+	function handleAddItems(item) {
+		setItems((items) => [...items, item]);
+	}
+
 	return (
 		<div className="app">
 			<Logo />
-			<Form />
-			<PackingList />
+			<Form onAddItems={handleAddItems} />
+			<PackingList items={items} />
 			<Stats />
 		</div>
 	);
@@ -21,7 +27,7 @@ function Logo() {
 	return <h1>🏝️ far away</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
 	const [description, setDescripton] = useState("");
 	const [quantity, setQuantitiy] = useState(1);
 
@@ -37,7 +43,7 @@ function Form() {
 			id: Date.now(),
 		};
 		console.log(newItem);
-
+		onAddItems(newItem);
 		setDescripton("");
 		setQuantitiy(1);
 	}
@@ -66,11 +72,11 @@ function Form() {
 	);
 }
 
-function PackingList() {
+function PackingList({ items }) {
 	return (
 		<div className="list">
 			<ul>
-				{initialItems.map((item) => (
+				{items.map((item) => (
 					<Item item={item} key={item.id} />
 				))}
 			</ul>
